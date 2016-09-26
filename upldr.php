01d71fa3432fd3ce92f7ea5bc8c0e8b265a526c6
247even <247even@users.noreply.github.com>
2016-09-26 13:46:51 +0200

    Create upldr.php

---
+++ upldr.php
@@ -0,0 +1,18 @@
+<?php
+
+foreach ($_FILES["files"] as $file) {
+	echo "hallo";
+	echo json_encode($file).'<br>';
+}
+/*
+foreach ($_FILES["pictures"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        // basename() may prevent filesystem traversal attacks;
+        // further validation/sanitation of the filename may be appropriate
+        $name = basename($_FILES["pictures"]["name"][$key]);
+        move_uploaded_file($tmp_name, "data/$name");
+    }
+}
+ */
+?>
